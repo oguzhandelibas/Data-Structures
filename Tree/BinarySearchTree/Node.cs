486c04b50f51ff8425ebc98d509e56fb0a174286
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-02-25 17:54:40 +0300

    +add: Node

---
+++ Tree/BinarySearchTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tree.BST
+{
+    public class Node<T>
+    {
+        public int Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public Node()
+        {
+
+        }
+        public Node(T value)
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+    }
+}
